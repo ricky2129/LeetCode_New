36ffb7183219364562ba65cf054872a9103f94aa
Ricky Makhija <96081477+ricky2129@users.noreply.github.com>
2024-08-18 21:44:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (49.53%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
-    void findMin(TreeNode* root, long long &mini) {
-        if (root == NULL) {
+    // void findMin(TreeNode* root, long long &mini) {
+    //     if (root == NULL) {
+    //         return; 
+    //     }
+    //     mini = min(mini, (long long)root->val);
+    //     findMin(root->left, mini);
+    //     findMin(root->right, mini);
+    // }
+
+    // void findMin2(TreeNode* root, long long &mini, long long &smin) {
+    //     if (root == NULL) {
+    //         return; 
+    //     }
+    //     if (root->val != mini) {
+    //         smin = min(smin, (long long)root->val);
+    //     }
+    //     findMin2(root->left, mini, smin);
+    //     findMin2(root->right, mini, smin);
+    // }
+
+    void solve(TreeNode* root, long long &mini, long long &smin){
+           if (root == NULL) {
             return; 
         }
         mini = min(mini, (long long)root->val);
-        findMin(root->left, mini);
-        findMin(root->right, mini);
-    }
-
-    void findMin2(TreeNode* root, long long &mini, long long &smin) {
-        if (root == NULL) {
-            return; 
-        }
-        if (root->val != mini) {
+             if (root->val != mini) {
             smin = min(smin, (long long)root->val);
         }
-        findMin2(root->left, mini, smin);
-        findMin2(root->right, mini, smin);
+
+        solve(root->left, mini,smin);
+        solve(root->right, mini,smin);
+
     }
 
     int findSecondMinimumValue(TreeNode* root) {
         long long mini = LLONG_MAX;  // use LLONG_MAX from <climits>
-        findMin(root, mini);
+        
         long long smin = LLONG_MAX;
-        findMin2(root, mini, smin);
+        solve(root, mini, smin);
 
         if (smin == LLONG_MAX) {
             return -1;
         } else {
             return smin;
         }
+
     }
 };
