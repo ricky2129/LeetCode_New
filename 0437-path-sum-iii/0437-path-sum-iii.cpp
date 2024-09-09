d5871e72c2c55f64420fd7ff12e8ec6bff83223d
Ricky Makhija <96081477+ricky2129@users.noreply.github.com>
2024-09-09 14:51:40 +0530

    Time: 20 ms (42%), Space: 17.8 MB (84.36%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int count = 0;
     void solve(TreeNode* root, long long targetSum, long long currSum, int& count) {
         if (root == NULL) {
             return;
@@ -23,14 +24,14 @@ public:
             return 0;
         }
 
-        int count = 0;
+        
         
         // Perform a DFS starting from the current node
         solve(root, targetSum, 0, count);
         
         // Recursively check paths starting from the left and right child nodes
-        count += pathSum(root->left, targetSum);
-        count += pathSum(root->right, targetSum);
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
 
         return count;
     }
